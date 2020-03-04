b933f6939b0c2809538d66cea2ec82cfb0822851
Harry Tung <honeytung@gmail.com>
2020-03-03 23:03:31 -0800

    Added Simple Title Screen

--- src/main/ui/main.java
+++ src/main/ui/main.java
@@ -3,6 +3,8 @@ package ui;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,6 +12,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -28,13 +35,39 @@ public class main extends Application {
     public void start(Stage primaryStage) throws Exception {
         mainWindow = primaryStage;
         initMenuScene();
+        mainWindow.setTitle("Snake Game");
         mainWindow.setScene(mainMenu);
         mainWindow.show();
     }
 
     private void initMenuScene() {
-        Group root = new Group();
-        mainMenu = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+        VBox titleScreen = new VBox();
+        VBox titleArea = new VBox();
+        VBox buttonArea = new VBox();
+        HBox optionsArea = new HBox();
+        Button SinglePlayer = new Button("SinglePlayer");
+        Button Computer = new Button("Computer");
 
+        // Label Properties
+        Label titleLabel = new Label("Snake Game");
+        titleLabel.setFont(new Font(40));
+        titleLabel.setAlignment(Pos.CENTER);
+
+        // VBox for titleArea
+        titleArea.getChildren().add(titleLabel);
+        titleArea.setPrefSize(WINDOW_WIDTH, WINDOW_HEIGHT / 2);
+        titleArea.setAlignment(Pos.CENTER);
+
+        // HBox for buttons
+        optionsArea.setSpacing(10);
+        optionsArea.getChildren().addAll(SinglePlayer, Computer);
+        optionsArea.setAlignment(Pos.CENTER);
+
+        // Set entire scene
+        buttonArea.getChildren().add(optionsArea);
+        titleScreen.getChildren().addAll(titleArea, buttonArea);
+        titleScreen.setPadding(new Insets(10));
+
+        mainMenu = new Scene(titleScreen, WINDOW_WIDTH, WINDOW_HEIGHT);
     }
 }
